c3daadaca179465f5de141433d22e10f316b977c
Your Name <you@example.com>
2023-07-23 23:18:26 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zael0vm.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+
+    const dreamCollegeCollections = client.db('dream-colleges').collection('colleges');
+
+    app.get('/colleges/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await dreamCollegeCollections.findOne(query);
+      res.send(result);
+    });
+    
+    app.get('/colleges', async (req, res) => {
+      const cursor = dreamCollegeCollections.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send('college is running');
+});
+
+app.listen(port, () => {
+  console.log(`college is running on port ${port}`);
+});
